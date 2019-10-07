0a090fc368aac4721a4be965b882f70664cd30cd
Boky <boky@treboada.info>
2019-10-07 11:07:41 +0200

    Update cartas.class.php
    pequeños cambios en la clase de baraja

--- cartas.class.php
+++ cartas.class.php
@@ -9,13 +9,14 @@ class baraja {
     public function __construct() {
         $this->palos = ['oro','espada','copa','basto'];
         $this->valores = ['1','2','3','4','5','6','7','10','11','12'];
-    }
-    public function mazo() {
         foreach ($this->palos as $palo) {
             foreach($this->valores as $valor) {
                 $this->mazo[] = array("palo"=>$palo, "valor"=>$valor);
             }
         }
+    }
+    public function mazo() {
+        # esta función servirá para crear nuevas barajas con diferentes palos.
         return $this->mazo;
     }
     public function mezcla() {
