0e22dda16dba5bba54652c450b6def61c78e8c50
Boky <boky@treboada.info>
2019-10-07 10:11:09 +0200

    Update cartas.php
    reparto de manos

--- cartas.php
+++ cartas.php
@@ -13,4 +13,32 @@ foreach ($palos as $palo) {
 }
 # mezclamos el mazo
 shuffle($mazo);
+
+
+#creamos los jugadores
+$jugadores = ['Heroe','Villano'];
+
+# creamos la mano
+$manos = array();
+
+#repartimos
+foreach ($jugadores as $jugador) {
+    #en este ejemplo repartiremos solo 3 cartas
+    $primera_carta = array_shift($mazo);
+    $segunda_carta = array_shift($mazo);
+    $tercera_carta = array_shift($mazo);
+
+    # calculando el valor de la mano
+    $valor_de_mano = $primera_carta['valor']+$segunda_carta['valor']+$tercera_carta['valor'];
+
+    # generando la mano para imprimir a posteriori el ganador
+    $manos[] = array(
+        "jugador" => $jugador,
+        "primera_carta"=>$primera_carta,
+        "segunda_carta"=>$segunda_carta,
+        "tercera_carta"=>$tercera_carta,
+        "valor"=>$valor_de_mano
+    );
+}
+
 ?>
