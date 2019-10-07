3059684543df167d0b77cf71a091628c265219ac
Boky <bokyone@gmail.com>
2019-10-07 10:07:52 +0200

    Update cartas.php
    simplificando el primer foreach

--- cartas.php
+++ cartas.php
@@ -7,11 +7,6 @@ $valores = ['1','2','3','4','5','6','7','10','11','12'];
 $mazo = array();
 foreach ($palos as $palo) {
     foreach ($valores as $valor) {
-        # condición especifica de este juego:
-        #   si palo es bastos o espadas el valor es negativo
-        if ($palo == "basto" OR $palo == "espada") { $valor = $valor*-1; }
-        # valor positivo para la ruta de imagen
-        $absvalor = abs($valor);
         # generando el mazo
         $mazo[] = array("palo"=>$palo, "valor"=>$valor);
     }
